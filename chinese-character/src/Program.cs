437f03a2f3e6254f46bfc609136d2b1b7fc0c0a8
Jackal <q5401103q@github.com>
2018-06-12 14:31:39 +0800

    add a tutorial about chinese character

---
+++ chinese-character/src/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.International.Converters.PinYinConverter;
+
+namespace ChineseCharacter_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "變";
+            Console.WriteLine("{0}共{1}画", str, GetStringStrokeNumber(str));
+
+            Console.Read();
+        }
+
+        public static int GetStringStrokeNumber(string str)
+        {
+            int count = 0;
+            char[] c = str.ToCharArray();
+            foreach(char temp in c)
+            {
+                count += ChineseChar.GetStrokeNumber(temp);
+            }
+            return count;
+        }
+    }
+}
