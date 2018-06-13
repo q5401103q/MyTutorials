730921a62a0d9516564b6611ddbc461427cb0ecb
q5401103q <q5401103q@github.com>
2018-06-13 14:01:14 +0800

    image processor tutorial

---
+++ image-processor/ImageProcessor/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ImageProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtImagePath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(105, 210);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "处理图片";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtImagePath
+            // 
+            this.txtImagePath.Location = new System.Drawing.Point(12, 43);
+            this.txtImagePath.Name = "txtImagePath";
+            this.txtImagePath.Size = new System.Drawing.Size(220, 21);
+            this.txtImagePath.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(238, 41);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(34, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = ">";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtImagePath);
+            this.Controls.Add(this.btnProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtImagePath;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ image-processor/ImageProcessor/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessor
+{
+    public partial class Form1 : Form
+    {
+        private readonly string path = "D:/images/";
+        private readonly string watermark = "www.github.com";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtImagePath.Text = dialog.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filename = txtImagePath.Text;
+            
+            Bitmap bitmap = new Bitmap(filename);
+            Graphics graphic = Graphics.FromImage(bitmap);
+            int width = bitmap.Width - 1;
+            int height = bitmap.Height - 1;
+
+            Color color;
+            Bitmap bitmap2 = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), PixelFormat.DontCare);
+            Bitmap bitmap3 = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), PixelFormat.DontCare);
+
+            //第一张图片，加文字
+            graphic.DrawImage(bitmap, new Rectangle(0, 0, width, height));
+            graphic.SmoothingMode = SmoothingMode.AntiAlias;
+            graphic.DrawString(watermark, new Font("黑体", 32.0f), Brushes.Red, 20.0f, 100.0f);
+            bitmap.Save(string.Format("{0}_01.jpg", path), ImageFormat.Jpeg);
+
+            //第二张图片，灰度处理
+            for (int i = width; i >= 0; i--)
+            {
+                for (int j = height; j >= 0; j--)
+                {
+                    //使用平均值法进行灰度处理   
+                    color = bitmap2.GetPixel(i, j);
+                    int middle = (color.R + color.G + color.B) / 3;
+                    Color colorResult = Color.FromArgb(255, middle, middle, middle);
+                    bitmap2.SetPixel(i, j, colorResult);
+
+                    //使用最大值法进行灰度处理
+                    //color = bitmap2.GetPixel(i, j);
+                    //int tmp = Math.Max(color.R, color.G);
+                    //int maxcolor = Math.Max(tmp, color.B);
+                    //Color colorResult = Color.FromArgb(255, maxcolor, maxcolor, maxcolor);
+                    //bitmap2.SetPixel(i, j, colorResult);
+                }
+            }
+            graphic.DrawImage(bitmap2, new Rectangle(0, 0, width, height));
+            bitmap2.Save(string.Format("{0}_02.jpg", path), ImageFormat.Jpeg);
+        }
+    }
+}
