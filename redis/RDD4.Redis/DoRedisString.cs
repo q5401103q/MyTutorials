ab974bd05d28086c780b8f4ae256dd4c573c38e5
q5401103q <q5401103q@github.com>
2018-06-13 11:59:06 +0800

    initial version of redis helper

---
+++ redis/RDD4.Redis/DoRedisString.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDD4.Redis
+{
+    public class DoRedisString : RedisBase
+    {
+        #region 赋值
+
+        /// <summary>
+        /// 设置key的value
+        /// </summary>
+        public bool Set(string key, string value)
+        {
+            return RedisBase.Core.Set<string>(key, value);
+        }
+
+        /// <summary>
+        /// 设置key的value并设置过期时间
+        /// </summary>
+        public bool Set(string key, string value, DateTime dt)
+        {
+            return RedisBase.Core.Set<string>(key, value, dt);
+        }
+
+        /// <summary>
+        /// 设置key的value并设置过期时间
+        /// </summary>
+        public bool Set(string key, string value, TimeSpan sp)
+        {
+            return RedisBase.Core.Set<string>(key, value, sp);
+        }
+
+        /// <summary>
+        /// 设置多个key/value
+        /// </summary>
+        public void Set(Dictionary<string, string> dic)
+        {
+            RedisBase.Core.SetAll(dic);
+        }
+
+        #endregion
+
+        #region 追加
+
+        /// <summary>
+        /// 在原有key的value值之后追加value
+        /// </summary>
+        public long Append(string key, string value)
+        {
+            return RedisBase.Core.AppendToValue(key, value);
+        }
+
+        #endregion
+
+        #region 删除
+
+        /// <summary>
+        /// 删除指定的key
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool Delete(string key)
+        {
+            return RedisBase.Core.Remove(key);
+        }
+
+        #endregion
+
+        #region 获取值
+
+        /// <summary>
+        /// 获取key的value值
+        /// </summary>
+        public string Get(string key)
+        {
+            return RedisBase.Core.Get<string>(key);
+        }
+
+        /// <summary>
+        /// 获取多个key的value值
+        /// </summary>
+        public List<string> Get(List<string> keys)
+        {
+            return RedisBase.Core.GetValues(keys);
+        }
+
+        /// <summary>
+        /// 获取多个key的value值
+        /// </summary>
+        public List<T> Get<T>(List<string> keys)
+        {
+            return RedisBase.Core.GetValues<T>(keys);
+        }
+
+        #endregion
+
+        #region 获取旧值赋上新值
+
+        /// <summary>
+        /// 获取旧值赋上新值
+        /// </summary>
+        public string GetAndSetValue(string key, string value)
+        {
+            return RedisBase.Core.GetAndSetEntry(key, value);
+        }
+
+        #endregion
+
+        #region 辅助方法
+
+        /// <summary>
+        /// 自增1，返回自增后的值
+        /// </summary>
+        public long Incr(string key)
+        {
+            return RedisBase.Core.IncrementValue(key);
+        }
+
+        /// <summary>
+        /// 自增count，返回自增后的值
+        /// </summary>
+        public double IncrBy(string key, int count)
+        {
+            return RedisBase.Core.IncrementValueBy(key, count);
+        }
+
+        /// <summary>
+        /// 自减1，返回自减后的值
+        /// </summary>
+        public long Decr(string key)
+        {
+            return RedisBase.Core.DecrementValue(key);
+        }
+
+        /// <summary>
+        /// 自减count ，返回自减后的值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public long DecrBy(string key, int count)
+        {
+            return RedisBase.Core.DecrementValueBy(key, count);
+        }
+
+        #endregion
+    }
+}
